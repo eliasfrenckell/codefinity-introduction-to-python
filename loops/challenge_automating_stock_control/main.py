d81733d204dfb88edf108ba873fd9e1bb48f8f58
eliasfrenckell <>
2026-04-26 11:34:48 +0000

    Challenge: Automating Stock Control

---
+++ loops/challenge_automating_stock_control/main.py
@@ -0,0 +1,29 @@
+# Initialize the inventory dictionary with stock details
+inventory = {
+    "Bread": [30, 50, 10, False],   # "Item": [current stock, minimum stock, restock quantity, on sale (True/False)]
+    "Eggs": [120, 200, 40, False],
+    "Milk": [60, 100, 20, False],
+    "Apples": [15, 50, 15, False]
+}
+
+
+discount_threshold = 100
+
+print("Processing started")
+
+for item in inventory:
+    print(f"Processing {item}")
+
+    current_stock = inventory[item][0]
+    minimum_stock = inventory[item][1]
+    restock_quantity = inventory[item][2]
+    sale_status = inventory[item][3]
+
+    while current_stock < minimum_stock:
+        current_stock += restock_quantity
+    inventory[item][0] = current_stock
+
+    if current_stock > discount_threshold and sale_status == False:
+        inventory[item][3] = True
+
+print("Processing completed")
